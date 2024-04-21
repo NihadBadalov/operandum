cce57c0e96b092a5c5d0f905c303fb3d6d7fdc6a
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-04-21 15:42:33 +0200

    feat: optimize to be ran by builtin functions

--- src/commands/decrypt.js
+++ src/commands/decrypt.js
@@ -2,35 +2,59 @@ import { intro, outro, text } from '@clack/prompts';
 import fs from 'node:fs';
 import { handleCancel } from '../utils/handleClackCancel.js';
 import { decryptAES256 } from '../crypto.js';
+import { parseINIFile } from '../parser.js';
 const chalk = (await import('chalk')).default;
 
 /**
  * @param {string} workDir Current working directory (cwd)
  * @param {string} filename File name to decrypt
+ * @param {string} pswd Provided password for decryption
  * @returns {Promise<void>}
  */
-export default async function decrypt(workDir, filename) {
-  if (!fs.existsSync(`${workDir}/${filename}`))
+export default async function decrypt(workDir, filename, pswd = '') {
+  if (filename.endsWith('/*')) {
+    const config = parseINIFile(`${workDir}/operandum.ini`);
+    if (!config)
+      return console.error(chalk.bgRed.white(`Error: in decrypt(): Unable to read operandum.ini`));
+
+    if (!fs.existsSync(workDir))
+      return console.error(chalk.bgRed.white(`Error: in decrypt(): Directory ${workDir} does not exist`));
+
+    if (filename.startsWith('./')) filename = filename.slice(2);
+    if (filename.endsWith('/*')) filename = filename.slice(0, -2);
+
+    const workDirFiles = fs
+      .readdirSync(`${workDir}/${config.tasks}/${filename}`)
+      .filter((file) => !fs.lstatSync(`${workDir}/${config.tasks}/${filename}/${file}`).isDirectory());
+
+    for (const file of workDirFiles) {
+      console.log('-', chalk.yellow('Decrypting file:'), `${workDir}/${config.tasks}/${filename}/${file}`);
+      await decrypt(workDir, `${config.tasks}/${filename}/${file}`, pswd);
+    }
+    return;
+  } else if (!fs.existsSync(`${workDir}/${filename}`))
     return console.error(chalk.bgRed.white(`Error: in decrypt(): File ${filename} does not exist`));
+
 
   const file = fs.readFileSync(`${workDir}/${filename}`, 'utf-8');
   if (!file)
     return console.error(chalk.bgRed.white(`Error: in decrypt(): Unable to read file ${filename}`));
 
-  intro(chalk.bgWhite.black(' operandum '));
+  if (!pswd) intro(chalk.bgWhite.black(' operandum '));
 
-  const password = await text({
+  const password = pswd || await text({
     message: 'Enter password to decrypt file',
     validate: (input) => {
       if (input.length < 1) return 'Password must be at least 1 character';
     },
   });
-  handleCancel(password);
+  if (!pswd) handleCancel(password);
 
   const decrypted = decryptAES256(file, password);
 
   fs.writeFileSync(`${workDir}/${filename}`, decrypted, 'utf-8');
-  outro(chalk.green(`File ${filename} decrypted successfully`));
+  if (!pswd) outro(chalk.green(`File ${filename} decrypted successfully`));
+  else console.log(chalk.green(`- File ${filename} decrypted successfully`));
 }
 
 const DESCRIPTION = 'AES-256-CBC decryption of a file';
--- src/commands/encrypt.js
+++ src/commands/encrypt.js
@@ -2,44 +2,68 @@ import { intro, outro, text } from '@clack/prompts';
 import fs from 'node:fs';
 import { handleCancel } from '../utils/handleClackCancel.js';
 import { encryptAES256 } from '../crypto.js';
+import { parseINIFile } from '../parser.js';
 const chalk = (await import('chalk')).default;
 
 /**
  * @param {string} workDir Current working directory (cwd)
  * @param {string} filename File name to encrypt
+ * @param {string} pswd Provided password for decryption
+ * @returns {Promise<void>}
  */
-export default async function encrypt(workDir, filename) {
-  if (!fs.existsSync(`${workDir}/${filename}`))
+export default async function encrypt(workDir, filename, pswd = '') {
+  if (filename.endsWith('/*')) {
+    const config = parseINIFile(`${workDir}/operandum.ini`);
+    if (!config)
+      return console.error(chalk.bgRed.white(`Error: in ecrypt(): Unable to read operandum.ini`));
+
+    if (!fs.existsSync(workDir))
+      return console.error(chalk.bgRed.white(`Error: in encrypt(): Directory ${workDir} does not exist`));
+
+    if (filename.startsWith('./')) filename = filename.slice(2);
+    if (filename.endsWith('/*')) filename = filename.slice(0, -2);
+
+    const workDirFiles = fs
+      .readdirSync(`${workDir}/${config.tasks}/${filename}`)
+      .filter((file) => !fs.lstatSync(`${workDir}/${config.tasks}/${filename}/${file}`).isDirectory());
+
+    for (const file of workDirFiles) {
+      console.log('-', chalk.yellow('Encrypting file:'), `${workDir}/${config.tasks}/${filename}/${file}`);
+      await encrypt(workDir, `${config.tasks}/${filename}/${file}`, pswd);
+    }
+    return;
+  } else if (!fs.existsSync(`${workDir}/${filename}`))
     return console.error(chalk.bgRed.white(`Error: in encrypt(): File ${filename} does not exist`));
+
 
   const file = fs.readFileSync(`${workDir}/${filename}`, 'utf-8');
   if (!file)
     return console.error(chalk.bgRed.white(`Error: in encrypt(): Unable to read file ${filename}`));
 
-  intro(chalk.bgWhite.black(' operandum '));
+  if (!pswd) intro(chalk.bgWhite.black(' operandum '));
 
-  const password = await text({
+  const password = pswd || await text({
     message: 'Enter password to encrypt file',
     validate: (input) => {
       if (input.length < 1) return 'Password must be at least 1 character';
     },
   });
-  handleCancel(password);
+  if (!pswd) handleCancel(password);
 
-  const confirmPass = await text({
+  const confirmPass = pswd || await text({
     message: 'Confirm password',
     validate: (input) => {
       if (input !== password) return 'Passwords do not match';
     },
   });
-  handleCancel(confirmPass);
+  if (!pswd) handleCancel(confirmPass);
 
   const encrypted = encryptAES256(file, password);
 
   fs.writeFileSync(`${workDir}/${filename}`, encrypted, 'utf-8');
-  outro(chalk.green(`File ${filename} encrypted successfully`));
+  if (!pswd) outro(chalk.green(`File ${filename} encrypted successfully`));
+  else console.log(chalk.green(`- File ${filename} encrypted successfully`));
 }
-
 
 const DESCRIPTION = 'AES-256-CBC encryption of a file';
 const USAGE = 'operandum encrypt <filename>';
