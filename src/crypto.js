d8d30f7c93e7d5cdb1a9bfbeb6e826b80376a6e1
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-04-21 19:55:54 +0200

    feat: error handling for crypto

--- src/crypto.js
+++ src/crypto.js
@@ -1,4 +1,5 @@
 const crypto = (await import('crypto')).default;
+const chalk = (await import('chalk')).default;
 
 /**
  * @param {string} text Text to encrypt
@@ -20,17 +21,24 @@ export function encryptAES256(text, password) {
  * @returns {string} Decrypted text
  */
 export function decryptAES256(text, password) {
-  if (!text.startsWith('OPERANDUM;'))
-    throw new Error('Error: in decryptAES256(): Invalid encrypted text; Text was not encrypted by operandum or is corrupted');
+  try {
+    if (!text.startsWith('OPERANDUM;'))
+      throw new Error('Error: in decryptAES256(): Invalid encrypted text; Text was not encrypted by operandum or is corrupted');
 
-  const parts = text.split(';');
-  if (parts.length !== 3)
-    throw new Error('Error: in decryptAES256(): Invalid encrypted text format; Expected IV part');
+    const parts = text.split(';');
+    if (parts.length !== 3)
+      throw new Error('Error: in decryptAES256(): Invalid encrypted text format; Expected IV part');
 
-  const key = crypto.scryptSync(password, 'salt', 32);
-  const iv = Buffer.from(parts[1], 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-  let decrypted = decipher.update(parts[2], 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return decrypted;
+    const key = crypto.scryptSync(password, 'salt', 32);
+    const iv = Buffer.from(parts[1], 'hex');
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    let decrypted = decipher.update(parts[2], 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+  } catch (error) {
+    if (error?.code === 'ERR_OSSL_BAD_DECRYPT') {
+      throw new Error(`Error: in decryptAES256(): ${chalk.bgRed.white('Invalid password')}`);
+    }
+    throw error;
+  }
 }
