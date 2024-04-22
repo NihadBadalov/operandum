b5a90f3ee4d3886343f69ad762879a32e7ec9afd
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-04-22 21:03:52 +0200

    feat: add stats to the bottom of help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,10 +1,15 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import { getBaseDotfileDirectory } from '../utils/getBaseDotfileDirectory.js';
+import { parseINIFile } from '../parser.js';
 const chalk = (await import('chalk')).default;
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-export default async function help() {
+/**
+ * @param {string} cwd Current working directory
+ */
+export default async function help(cwd) {
   let helpString = '\nUsage:\n  operandum <command>\n\nCommands:';
   const filenames = fs.readdirSync(__dirname);
   filenames.sort();
@@ -21,7 +26,37 @@ export default async function help() {
   }
 
   console.log(helpString);
-  // TODO: managing N tasks, N dotfiles
+  const baseDir = getBaseDotfileDirectory(cwd);
+  if (baseDir) {
+    const config = parseINIFile(`${baseDir}/operandum.ini`);
+    if (!config) return;
+
+    const dotfilesDir = config?.dotfiles;
+    const tasksDir = config?.tasks;
+    let dotfilesCount = 0;
+    let tasksCount = 0;
+    if (dotfilesDir) {
+      const dotfiles = fs.readdirSync(`${baseDir}/${dotfilesDir}`);
+      dotfilesCount = dotfiles.length;
+    }
+    if (tasksDir) {
+      const tasks = fs
+        .readdirSync(`${baseDir}/${tasksDir}`, { withFileTypes: true });
+      tasksCount = tasks.length;
+    }
+
+    console.log();
+    if (dotfilesCount > 0 && tasksCount > 0) {
+      console.log(`Managing ${dotfilesCount} Dotfiles and ${tasksCount} Tasks.`);
+    } else if (dotfilesCount > 0 && tasksCount < 0) {
+      console.log(`Managing ${dotfilesCount} Dotfiles.`);
+    } else if (dotfilesCount < 0 && tasksCount > 0) {
+      console.log(`Managing ${tasksCount} Tasks.`);
+    } else {
+      console.log(chalk.green('Start managing your Dotfiles, Tasks, and Scripts now!'));
+      console.log(chalk.underline.green('https://github.com/NihadBadalov/operandum#initialization'));
+    }
+  }
 }
 
 const DESCRIPTION = 'Shows a help message for operandum commands';
